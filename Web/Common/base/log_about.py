e910924a2470ed13406b2df5acf1d33ca0f9f47e
1322024409@qq.com <contractor8@yingren.cn>
2023-07-04 16:04:04 +0800

    add python scripts of web

---
+++ Web/Common/base/log_about.py
@@ -0,0 +1,14 @@
+import logging
+
+logging.basicConfig(level=logging.ERROR)
+
+logging.debug("this is a debug message")
+
+logging.info("this is a info message")
+
+logging.error("this is a error message")
+
+logging.warning("this is a warning message")
+
+logging.critical("this is a critical message")
+
